a2b9456e6aaa6350a01208be53940b27a7098975
sagarwarade <sagarwarade1997@gmail.com>
2024-05-20 14:08:36 +0530

    Fixed the code

--- src/app/ngx-currency-example/ngx-currency-example.component.ts
+++ src/app/ngx-currency-example/ngx-currency-example.component.ts
@@ -10,18 +10,23 @@ export class NgxCurrencyExampleComponent {
   amount1: any;
   amount2: any;
   showPrefixSuffix: boolean = true;
+  NgxCurrencyInputMode:any;
   customCurrencyOptions: NgxCurrencyConfig = {
     align: "right",
     allowNegative: false,
     allowZero: false,
     decimal: ".",
     precision: 2,
-    prefix: "$",
-    suffix: "",
+    prefix: "₹",
+    suffix: "r",
     thousands: ",",
     nullable: false,
     min: null,
     max: null,
     inputMode: NgxCurrencyInputMode.Natural
   };
+
+  constructor(){
+    this.NgxCurrencyInputMode = NgxCurrencyInputMode
+  }
 }
